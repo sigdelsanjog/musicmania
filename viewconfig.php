83dd53f33691ff9dd4ebef9663554a27ae085a82
algosig <sigsandy02@gmail.com>
2016-04-04 16:10:01 +0545

    Created file:viewconfig.php
    Added file viewconfig, where configuration can be managed

---
+++ viewconfig.php
@@ -0,0 +1,9 @@
+<?php
+$database = "song";  // the name of the database.
+$server = "localhost";  // server to connect to.
+$db_user = "root";  // mysql username to access the database with.
+$db_pass = "";  // mysql password to access the database with.
+  // the table that this script will set up and use.
+$link = mysql_connect($server, $db_user, $db_pass);
+mysql_select_db($database,$link);
+?>
